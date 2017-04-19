907b5773f4af429e22c23aaf95abde0aa83fb022
Stéphane Gully <stephane.gully@gmail.com>
2017-04-19 15:23:52 +0200

    first look and feel for the small dynamic bibcnrs button

--- app/scripts/bibcnrs-button.js
+++ app/scripts/bibcnrs-button.js
@@ -1,6 +1,8 @@
+import queue from 'async/queue';
+
 var $ = require('jquery');
 
-function d(obj) { return JSON.stringify(obj); };
+function debug(obj) { return JSON.stringify(obj); };
 
 var util = require('util');
 
@@ -11,9 +13,51 @@ function BibCNRSButton(options) {
   options          = options || {};
   self.myopt       = options.myopt || 'coucou';
   self.doiPattern = /\/\/(dx\.doi\.org|doi\.acm\.org|dx\.crossref\.org).*\/(10\..*(\/|%2(F|f)).*)/;
+  
+  // handle found DOI in the DOM asynchrononsly
+  // to be gentle with the web browser
+  self.queueConcurrency = 1;
+  self.queue = queue(
+    (btnData, cb) => {
+      self.checkIfDoiIsAvailable(btnData, function (found) {
+        if (found) {
+          self.tryToHookAButton(btnData, cb);
+        } else {
+          return cb();
+        }
+      });
+    },
+    self.queueConcurrency
+  );
 }
 util.inherits(BibCNRSButton, EventEmitter);
 module.exports = BibCNRSButton;
+
+/**
+ * Checks if the DOI is available in the subscribed 
+ * BibCNRS packages thanks to EBSCO FTF
+ */
+BibCNRSButton.prototype.checkIfDoiIsAvailable = function (btnData, cb) {
+  var self = this;
+  //console.log('CHECKING DOI AVAILABILITY', btnData.foundDoi);
+  if (btnData.foundDoi == '10.1103/PhysRevLett.98.175302') {
+    return cb(1);      
+  } else {
+    return cb(0);
+  }
+};
+
+/**
+ * Hook a HTML button in the web page where 
+ * the DOI has been found .
+ */
+BibCNRSButton.prototype.tryToHookAButton = function (btnData, cb) {
+  var self = this;
+  console.log('HOOK A BUTTON ON THIS DOI', btnData.foundDoi);
+  // btnData
+  $('<a target="_blank" href="" class="bibcnrs-button-link"><span class="bibcnrs-button-box">INEE</span><span class="bibcnrs-button-icon"></span></a>').appendTo(btnData.domElt.parentNode);
+  return cb();
+};
 
 BibCNRSButton.prototype.myAction = function (data) {
   var self = this;
@@ -31,7 +75,8 @@ BibCNRSButton.prototype.hrefWalker = function () {
       let matches = elt.href.match(self.doiPattern);
       if (matches && matches[2]) {
         let doi = matches[2];
-        console.log('DOI FOUND', doi);
+        //console.log('DOI FOUND', doi);
+        self.queue.push({ foundDoi: doi, domElt: elt });
       }
     }, 10);
   });
