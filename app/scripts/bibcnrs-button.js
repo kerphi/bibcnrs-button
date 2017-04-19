7157063629d2032d617fd71c6e4bc7fc4f7796b0
Stéphane Gully <stephane.gully@gmail.com>
2017-04-19 15:36:02 +0200

    better DOM for the button

--- app/scripts/bibcnrs-button.js
+++ app/scripts/bibcnrs-button.js
@@ -19,9 +19,9 @@ function BibCNRSButton(options) {
   self.queueConcurrency = 1;
   self.queue = queue(
     (btnData, cb) => {
-      self.checkIfDoiIsAvailable(btnData, function (found) {
+      self.checkIfDoiIsAvailable(btnData, function (found, btnData2) {
         if (found) {
-          self.tryToHookAButton(btnData, cb);
+          self.tryToHookAButton(btnData2, cb);
         } else {
           return cb();
         }
@@ -41,7 +41,11 @@ BibCNRSButton.prototype.checkIfDoiIsAvailable = function (btnData, cb) {
   var self = this;
   //console.log('CHECKING DOI AVAILABILITY', btnData.foundDoi);
   if (btnData.foundDoi == '10.1103/PhysRevLett.98.175302') {
-    return cb(1);      
+
+
+    // TODO check if the DOI is in the bibcnrs package
+
+    return cb(1, Object.assign({}, btnData, {btnUrl: 'http://www.google.fr'}));
   } else {
     return cb(0);
   }
@@ -53,9 +57,15 @@ BibCNRSButton.prototype.checkIfDoiIsAvailable = function (btnData, cb) {
  */
 BibCNRSButton.prototype.tryToHookAButton = function (btnData, cb) {
   var self = this;
-  console.log('HOOK A BUTTON ON THIS DOI', btnData.foundDoi);
-  // btnData
-  $('<a target="_blank" href="" class="bibcnrs-button-link"><span class="bibcnrs-button-box">INEE</span><span class="bibcnrs-button-icon"></span></a>').appendTo(btnData.domElt.parentNode);
+  console.log('HOOK A BUTTON ON THIS DOI', btnData.foundDoi, btnData.btnUrl);
+
+  var domBtnLink = $('<a target="_blank" href="" class="bibcnrs-button-link"></a>').attr('href', btnData.btnUrl);
+  var domBtnBox  = $('<span class="bibcnrs-button-box"></span').text('INEE');
+  var domBtnIcon = $('<span class="bibcnrs-button-icon"></span>');
+
+  domBtnLink.append(domBtnBox);
+  domBtnLink.append(domBtnIcon);
+  domBtnLink.appendTo(btnData.domElt.parentNode);
   return cb();
 };
 
