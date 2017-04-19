b4f9dcf0ed9712defd12a26ca9ff9fd9ecbd8baa
Stéphane Gully <stephane.gully@gmail.com>
2017-04-19 16:08:18 +0200

    first implementation with ajax request to EBSCO FTF

--- app/scripts/bibcnrs-button.js
+++ app/scripts/bibcnrs-button.js
@@ -12,7 +12,7 @@ function BibCNRSButton(options) {
 
   options          = options || {};
   self.myopt       = options.myopt || 'coucou';
-  self.doiPattern = /\/\/(dx\.doi\.org|doi\.acm\.org|dx\.crossref\.org).*\/(10\..*(\/|%2(F|f)).*)/;
+  self.doiPattern = /\/\/(doi\.org|dx\.doi\.org|doi\.acm\.org|dx\.crossref\.org).*\/(10\..*(\/|%2(F|f)).*)/;
   
   // handle found DOI in the DOM asynchrononsly
   // to be gentle with the web browser
@@ -39,16 +39,39 @@ module.exports = BibCNRSButton;
  */
 BibCNRSButton.prototype.checkIfDoiIsAvailable = function (btnData, cb) {
   var self = this;
-  //console.log('CHECKING DOI AVAILABILITY', btnData.foundDoi);
-  if (btnData.foundDoi == '10.1103/PhysRevLett.98.175302') {
 
+  // if (btnData.foundDoi !== '10.1103/PhysRevLett.98.175302') {
+  //   return cb(0);
+  // }
 
-    // TODO check if the DOI is in the bibcnrs package
+  // btnData.foundDoi = '10.1007/s00701-016-2835-z';
 
-    return cb(1, Object.assign({}, btnData, {btnUrl: 'http://www.google.fr'}));
-  } else {
-    return cb(0);
-  }
+  // matching url:
+  // http://search.ebscohost.com/login.aspx?authtype=guest&custid=ns257146&groupid=main&profile=ftf&id=DOI:10.1007/s00701-016-2835-z&direct=true&site=ftf-live
+  $.ajax({
+    url: 'http://search.ebscohost.com/login.aspx?authtype=guest&custid=ns257146&groupid=main&profile=ftf&id=DOI:' + btnData.foundDoi + '&direct=true&site=ftf-live',
+    success : function(data) {
+      var proxyUrl = $(data).find('a[data-auto=guest-login-link]').attr('href');
+      proxyUrl = proxyUrl.split('?url=')[0] + '?url=';
+
+      var linkoutUrl = $(data).find('div.ftf-results a[data-auto=menu-link]').attr('href');
+//      linkoutUrl = 'http://resolver.ebscohost.com' + linkoutUrl;
+      
+      if (linkoutUrl) {
+        console.log('DOI IS AVAILABLE', linkoutUrl, proxyUrl + this.url);
+        return cb(1, Object.assign({}, btnData, { btnUrl: proxyUrl + this.url }));
+        return 
+      } else {
+        console.log('DOI IS NOT AVAILABLE', linkoutUrl, this.url);
+        return cb(0);
+      }
+    },
+    error: function(jqXHR, textStatus, errorThrown) {
+      console.log('AJAX ERROR', jqXHR, textStatus, errorThrown);
+      return cb(0);
+    }
+  });
+
 };
 
 /**
@@ -69,12 +92,6 @@ BibCNRSButton.prototype.tryToHookAButton = function (btnData, cb) {
   return cb();
 };
 
-BibCNRSButton.prototype.myAction = function (data) {
-  var self = this;
-  console.log('MY ACTION', data);
-  $('body').attr('data-ok', 'test');
-  $('p').css('color', 'red');
-};
 
 BibCNRSButton.prototype.hrefWalker = function () {
   var self = this;
@@ -85,7 +102,7 @@ BibCNRSButton.prototype.hrefWalker = function () {
       let matches = elt.href.match(self.doiPattern);
       if (matches && matches[2]) {
         let doi = matches[2];
-        //console.log('DOI FOUND', doi);
+        console.log('DOI FOUND', doi);
         self.queue.push({ foundDoi: doi, domElt: elt });
       }
     }, 10);
@@ -94,3 +111,4 @@ BibCNRSButton.prototype.hrefWalker = function () {
 
 
 module.exports = BibCNRSButton;
+
