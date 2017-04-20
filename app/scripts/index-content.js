8466b89ce5a5113c413b446d803f1a325f7bd908
Stéphane Gully <stephane.gully@gmail.com>
2017-04-20 19:01:56 +0200

    walker2 first try inspired by istex one

--- app/scripts/index-content.js
+++ app/scripts/index-content.js
@@ -4,5 +4,5 @@ var BibCNRSButton = require('./bibcnrs-button.js');
 
 var bcb = new BibCNRSButton();
 bcb.hrefWalker(document.body);
-bcb.textWalker(document.body);
+bcb.textWalker2(document.body);
 bcb.watchDomModifications();
